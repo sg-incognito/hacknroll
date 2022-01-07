241f6139bbadfc33b43588b04bc47ccbdf0f71fb
sg-incognito <joshuatech05@gmail.com>
2022-01-07 14:23:10 +0800

    Updated Templates 2

--- js/autoSaver.js
+++ js/autoSaver.js
@@ -1,3 +1,16 @@
+console.log("You are autorunning!");
+
+/*
 // IMPLEMENT SAVE FUNCTION
-console.log("Detected Form Page, Run Autosave if Enabled");
-// TODO AUTOSAVE
+console.log("Detected Form Page, Ask user for restore");
+var r=confirm("Restore previously stored form data?");
+if (r==true) {
+    // Restore Data
+    console.log("Restoring Data Now.");
+    // TODO: Implement Restore Function from Storage Map
+} else {
+    // Do nothing
+    console.log("No data to restore.");
+}
+
+*/
