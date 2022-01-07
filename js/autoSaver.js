3ac1721fcda2ffd4d26dcb914b891a892a57088f
sg-incognito <joshuatech05@gmail.com>
2022-01-07 14:35:29 +0800

    Added TODO

--- js/autoSaver.js
+++ js/autoSaver.js
@@ -1,16 +1,29 @@
-console.log("You are autorunning!");
 
-/*
-// IMPLEMENT SAVE FUNCTION
-console.log("Detected Form Page, Ask user for restore");
-var r=confirm("Restore previously stored form data?");
-if (r==true) {
-    // Restore Data
-    console.log("Restoring Data Now.");
-    // TODO: Implement Restore Function from Storage Map
+// IMPLEMENT AUTO-RESTORE FUNCTION FOR INITIAL PAGE LOAD
+console.log("Detected Form Page, Check if Save Exists");
+if (true) { // TODO: CHANGE TO CHECK IF SAVE FILE EXISTS IN CHROME STORAGE!
+    var r=confirm("Restore previously stored form data?");
+    if (r==true) {
+        // Restore Data
+        console.log("Data Restore Requested.");
+        // TODO: Implement Restore Function from Storage Map
+    } else {
+        // Do nothing
+        console.log("User has declined form restore.");
+    }
 } else {
-    // Do nothing
-    console.log("No data to restore.");
+    console.log("Save file does not exist.");
 }
 
-*/
+// IMPLEMENT AUTO-SAVE FUNCTION
+setInterval(saveContent, 5000);
+
+function saveContent() { // Runs every 5 Seconds
+    try {
+        console.log("Saving Form Data...");
+        // TODO: Implement Save Function to Storage Map
+    } catch (error) {
+        console.error("[Govtech Autosave] Error Saving Form Data: " + error);
+    }
+    console.log("Saved Successfully.");
+}
